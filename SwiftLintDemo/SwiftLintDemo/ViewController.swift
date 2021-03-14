776f5d4642581e7cfb4db591151fb26d5706b6c5
Dat, Vu Ba Tat <dat.vbt@homecredit.vn>
2021-03-14 10:36:32 +0700

    init project

---
+++ SwiftLintDemo/SwiftLintDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftLintDemo
+//
+//  Created by Dat, Vu Ba Tat on 3/14/21.
+//  Copyright © 2021 Dat, Vu Ba Tat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+    var TestView: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
